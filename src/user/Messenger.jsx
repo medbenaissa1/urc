b85fd9437e00be79872703fc115748424b21a7b1
Mohammed Benaissa <halanmyanime@gmail.com>
2025-10-28 23:42:19 +0100

    test de messages avec à distance avec Jaouad en temps réel  et ajout de l'icone déconnexion lors de la messagerie

--- src/user/Messenger.jsx
+++ src/user/Messenger.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useChat } from "../store/chat";
+import { useSession } from "../store/session";
 import { UsersList } from "./UsersList";
 import { MessagesList } from "./MessagesList";
-import { Box, Divider } from "@mui/material";
+import { Box, Divider, IconButton, Tooltip } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export function Messenger() {
   const navigate = useNavigate();
@@ -13,6 +15,7 @@ export function Messenger() {
   const selectUser = useChat((s) => s.selectUser);
   const startAutoRefresh = useChat((s) => s.startAutoRefresh);
   const stopAutoRefresh = useChat((s) => s.stopAutoRefresh);
+  const logout = useSession((s) => s.logout);
 
   // Load users on mount
   useEffect(() => {
@@ -35,9 +38,54 @@ export function Messenger() {
     return () => stopAutoRefresh();
   }, [userId, selectUser, startAutoRefresh, stopAutoRefresh]);
 
+  const handleLogout = () => {
+    stopAutoRefresh();
+    logout();
+    navigate("/login", { replace: true });
+  };
+
   return (
     <Box sx={{ display: "flex", height: "100vh", bgcolor: "background.default" }}>
-      <UsersList />
+      {/* --- Sidebar (Users list + logout) --- */}
+      <Box
+        sx={{
+          width: 280,
+          display: "flex",
+          flexDirection: "column",
+          borderRight: "1px solid #e0e0e0",
+          bgcolor: "white",
+        }}
+      >
+        <Box sx={{ flex: 1, overflowY: "auto" }}>
+          <UsersList />
+        </Box>
+
+        {/* Logout button */}
+        <Box
+          sx={{
+            borderTop: "1px solid #eee",
+            p: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Tooltip title="Déconnexion">
+            <IconButton
+              color="error"
+              onClick={handleLogout}
+              sx={{
+                bgcolor: "#fdecea",
+                "&:hover": { bgcolor: "#f8d7da" },
+              }}
+            >
+              <LogoutIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Box>
+
+      {/* --- Chat area --- */}
       <Divider orientation="vertical" flexItem />
       <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
         <MessagesList />
